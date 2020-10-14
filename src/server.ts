711d0e338d599510984181ea781fa0c85d0188be
joaolobao380 <victor_jv380@hotmail.com>
2020-10-14 02:03:25 -0300

    create: models and migrations

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import './database/connection';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('teste');
+});
+
+app.listen(3333);
